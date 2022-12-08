e6af134d7c8642b5dcd4fd27ceb3cb6b2d6198eb
Gabriel-Atsok <115570790+Gabriel-Atsok@users.noreply.github.com>
2022-12-07 22:15:20 -0300

    07122022

--- sistema/urls.py
+++ sistema/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',  ('appacademico.urls')),
+    path('', include ('appacademico.urls')),
 ]
 
 if settings.DEBUG:
