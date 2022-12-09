7d9b305e063224f2f2bc98519cacc4d0eaa412e5
Flavio-FH <118846177+Flavio-FH@users.noreply.github.com>
2022-12-08 22:46:01 -0300

    08/12/2022
    22:44

--- appacademico/forms.py
+++ appacademico/forms.py
@@ -1,17 +1,3 @@
 from django.db import models
 
-class Usuario(models.Model):
-    nome = models.CharField(max_length=150)
-    
-    def __str__(self):
-        return self.nome
-
-class Secretario(Usuario):
-    pass
-
-class Professor(Usuario):
-    pass
-
-class Aluno(Usuario):
-    pass
 
--- appacademico/models.py
+++ appacademico/models.py
@@ -1,1 +1,34 @@
 from django.db import models
+
+
+class Usuario(models.Model):
+    matricula=models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=150)  
+    cpf=models.CharField(max_length=15)
+    telefone=models.IntegerField(max_length=11)
+    email=models.EmailField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+class Secretario(Usuario):
+    pass
+
+class Professor(Usuario):
+    disciplina_id=models.IntegerField(primary_key=True)
+    turma_id=models.CharField(primary_key=True)
+    
+
+class Aluno(Usuario):
+    disciplina_id=models.IntegerField(primary_key=True)
+    turma_id=models.CharField(primary_key=True)
+    
+
+class Disciplina:
+    nome=models.CharField(max_length=50)
+    disciplina_id=models.IntegerField(primary_key=True)
+    turma_id=models.CharField(primary_key=True)
+
+class Turma:
+
+    disciplina_id=models.ForeignKey(Professor,Disciplina,on_delete=models.PROTECT)
