e5a7afdd648cccb59c9fe7e6d264c1d682927170
Gabriel-Atsok <115570790+Gabriel-Atsok@users.noreply.github.com>
2022-12-12 15:05:03 -0300

    12/12/2022 15:04

---
+++ contas/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [ 
+    path('', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('edit_cadastro/', views.edit_cadastro, name='edit_cadastro'),
+]
